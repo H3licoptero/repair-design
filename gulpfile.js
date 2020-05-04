14e8dc552ee224150aac609228d6d65a04bb560b
Vitaliy Nikolaiev <helicoptero3345@gmail.com>
2020-05-04 17:20:43 +0300

    include gulp-clean-css

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,27 @@
 
 const gulp = require("gulp");
 const browserSync = require("browser-sync").create();
+const cleanCSS = require("gulp-clean-css");
 
-gulp.task("hello", function(done) {
-  console.log('Привет, Мир!');
+gulp.task("hello", function (done) {
+  console.log("Привет, Мир!");
   done();
 });
 
 // Static server
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./src"
-        }
-    });
-    gulp.watch("./src*.html").on("change", browserSync.reload);
+gulp.task("browser-sync", function () {
+  browserSync.init({
+    server: {
+      baseDir: "./src",
+    },
+  });
+  gulp.watch("./src*.html").on("change", browserSync.reload);
 });
+
+// minify css
+gulp.task("minify-css", () => {
+return gulp.src("./src/style/*.css")
+  .pipe(cleanCSS())
+  .pipe(gulp.dest("dist"));
+});
+
