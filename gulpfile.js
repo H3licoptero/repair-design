7e8bfbe851efcd17ee2d5e479eb96bde28a31b0a
Vitaliy Nikolaiev <helicoptero3345@gmail.com>
2020-05-06 23:24:06 +0300

    add base files in sass(normalize, grid, variables, template, header, interface)

--- gulpfile.js
+++ gulpfile.js
@@ -8,32 +8,32 @@ const sass = require("gulp-sass");
 
 // Compile sass into CSS & auto-inject into browsers
  function serveSass() {
-    return src("./sass/*.sass")
+    return src("./src/sass/*.sass")
         .pipe(sass())
-        .pipe(dest("app/css"))
+        .pipe(dest("./src/style"))
         .pipe(browserSync.stream());
+}
+
+// minify css
+function minCss() {
+return src("./src/style/*.css")
+  .pipe(cleanCSS())
+  .pipe(rename({ suffix: ".min" }))
+  .pipe(dest("dist/style"));
 }
 
 // Static server
 function bs() {
+  serveSass();
+  minCss();
   browserSync.init({
     server: {
       baseDir: "./src",
     },
   });
   watch("./src/*.html").on("change", browserSync.reload);
-  watch("./sass/**/*.sass", serveSass);
+  watch("./src/sass/**/*.sass", serveSass);
   watch("./js/*.js");
 }
-
-// minify css
-function minCss() {
-return  src("./src/style/*.css")
-  .pipe(cleanCSS())
-  .pipe(rename({ suffix: ".min" }))
-  .pipe(dest("dist/style"));
-}
-
-
 
 exports.serve = bs;
