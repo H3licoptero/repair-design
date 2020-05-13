cff47445c4dd089979cccd4a8d8cec94bfee22da
Vitaliy Nikolaiev <helicoptero3345@gmail.com>
2020-05-13 23:34:59 +0300

    feat: add js functions for modal

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  "use strict";
+  const modal = document.querySelector('.modal');
+  const modalBtn = document.querySelectorAll('[data-toggle="modal"]'); 
+  const closeBtn = document.querySelector('.modal__close');
+  const switchModal = () => {
+    modal.classList.toggle("modal--visible");
+  };
+  
+  modalBtn.forEach((elem) => {
+    elem.addEventListener('click', switchModal);
+  });
+  
+  closeBtn.addEventListener('click', switchModal);
+  
+});
