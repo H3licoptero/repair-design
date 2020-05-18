09da60cfec2b233b516564e867dd3b88642c893f
Vitaliy Nikolaiev <helicoptero3345@gmail.com>
2020-05-18 20:35:29 +0300

    feat: add swiper-slider, styles for swiper and js file

--- src/js/main.js
+++ src/js/main.js
@@ -41,3 +41,19 @@ $(document).ready(function() {
     });
 
 });
+
+let mySwiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  // pagination: {
+  //   el: ".swiper-pagination",
+  // },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
