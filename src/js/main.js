1f0288f1b1d8d77541d6f2ac712523dcfb41c55b
Vitaliy Nikolaiev <helicoptero3345@gmail.com>
2020-05-24 17:16:55 +0300

    feat: add validation in all forms

--- src/js/main.js
+++ src/js/main.js
@@ -87,4 +87,90 @@ $(document).ready(function () {
   bullets.css("top", bullets.height() - 40);
 
   new WOW().init();
+
+  // валидация формы
+  $(".modal__form").validate({
+    errorClass: "modal__invalid",
+    rules: {
+      // правило для поля "введите имя"
+      userName: {
+        required: true,
+        minlength: 2,
+        maxlength: 15,
+      },
+      // правило для телефона
+      userPhone: "required",
+      // правила для поля "email"
+      userEmail: {
+        required: true,
+        email: true,
+      },
+    },
+    // сообщения при ошибках
+    messages: {
+      userName: {
+        required: "Это поле обязательно для ввода",
+        minlength: "Имя должно содержать не менее 2-х символов",
+        maxlength: "Имя должно содержать не более 15-ти символов",
+      },
+      userPhone: "Укажите свой номер телефона",
+      userEmail: {
+        required: "Обязательно укажите email",
+        email: "Введите в формате: name@domain.com",
+      },
+    },
+  });  
+  $(".footer__form").validate({
+    errorClass: "footer__invalid",
+    rules: {
+      footerName: {
+        required: true,
+        minlength: 2,
+        maxlength: 15,
+      },
+      footerPhone: {
+        required: true,
+      },
+      footerQuestion: {
+        required: true,
+      },
+    },
+    // сообщения при ошибках
+    messages: {
+      footerName: {
+        required: "Это поле обязательно для ввода",
+        minlength: "Имя должно содержать не менее 2-х символов",
+        maxlength: "Имя должно содержать не более 15-ти символов",
+      },
+      footerPhone: "Укажите свой номер телефона",
+      footerQuestion: {
+        required: "Напишите свой вопрос",
+      },
+    },
+  });
+  $(".control__form").validate({
+    errorClass: "control__invalid",
+    rules: {
+      // правило для поля "введите имя"
+      controlName: {
+        required: true,
+        minlength: 2,
+        maxlength: 15,
+      },
+      // правило для телефона
+      controlPhone: "required",
+
+    },
+    // сообщения при ошибках
+    messages: {
+      controlName: {
+        required: "Это поле обязательно для ввода",
+        minlength: "Имя должно содержать не менее 2-х символов",
+        maxlength: "Имя должно содержать не более 15-ти символов",
+      },
+      controlPhone: "Укажите свой номер телефона",
+    },
+  }); 
+  // маска для телефона
+  $("[type=tel]").mask("+7(000)000-00-00", {placeholder: "+7(___) ___-__-__"});
 });
