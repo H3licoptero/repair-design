2c92427446691ca49a8736692e19afef8f7f0d8e
Vitaliy Nikolaiev <helicoptero3345@gmail.com>
2020-05-27 18:35:18 +0300

    add gulpfiles without tinyPng

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 "use strict";
 
-const {src, dest, watch} = require("gulp");
+const {src, dest, watch, series} = require("gulp");
 const browserSync = require("browser-sync").create();
 const cleanCSS = require("gulp-clean-css");
 const rename = require("gulp-rename");
 const sass = require("gulp-sass");
 const autoprefixer = require("gulp-autoprefixer");
+const minify = require("gulp-minify");
+const htmlmin = require("gulp-htmlmin");
+const tinypng = require("gulp-tinypng-compress");
 
 
 // Compile sass into CSS & auto-inject into browsers
@@ -44,4 +47,53 @@ function bs() {
   watch("./js/*.js");
 }
 
+function buildCSS(done) {
+  src("./src/style/**.css")
+    .pipe(cleanCSS({ compatibility: "ie8" }))
+    .pipe(dest("dist/style"));
+  done();
+}
+
+function buildJS(done) {
+  src(["./src/js/**.js", "!./src/js/**.min.js"])
+    .pipe(
+      minify({
+        ext: {
+          min: ".js",
+        },
+      })
+    )
+    .pipe(dest("dist/js"));
+  src("./src/js/**.min.js")
+    .pipe(dest("dist/js/"));
+  done();
+}
+
+function buildHTML(done) {
+  src("./src/**.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(dest("dist/"));
+  done();
+}
+
+function buildPHP(done) {
+  src("./src/**.php")
+    .pipe(dest("dist"));
+  src("./src/PHPmailer/**.php")
+    .pipe(dest("dist/PHPmailer"));
+  done();
+}
+
+function buildFonts(done) {
+  src("./src/fonts/futura/**")
+  .pipe(dest("dist/fonts/futura"));
+  done();
+}
+
+function buildImg(done) {
+  done();
+}
+
+
 exports.serve = bs;
+exports.build = series(buildCSS, buildJS, buildHTML, buildPHP, buildFonts);
